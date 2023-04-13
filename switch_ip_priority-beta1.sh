adc4e306cac5d8769c3780f185481f92e27d39f7
Mr.Hokusai <33373488+younasiqw@users.noreply.github.com>
2023-04-13 21:20:02 +0800

    Update switch_ip_priority-beta1.sh

--- switch_ip_priority-beta1.sh
+++ switch_ip_priority-beta1.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# 获取当前系统所有网卡名称
-ifconfig | grep mtu | awk '{print $1}' > ./nics.txt
-
 # 定义颜色常量
 green="\e[32m"
 red="\e[31m"
@@ -16,49 +13,43 @@ do
     # 显示菜单
     echo -e "${yellow}歡迎使用 IP 協議優先級選擇工具${plain}"
     echo "----------------------------------------"
-    echo -e "${green}當前可用網卡列表${plain}"
-    cat nics.txt
+    echo -e "${green}當前可用網卡列表：${plain}"
+    ifconfig | grep mtu | awk '{print $1}'
     echo "----------------------------------------"
-    if grep -q "precedence 1" /etc/gai.conf; then
-        echo -e "${green}當前傾向使用 IPv4${plain}"
-    elif grep -q "precedence 2" /etc/gai.conf; then
-        echo -e "${green}當前傾向使用 IPv6${plain}"
-    else
-        echo -e "${green}未設置 IP 協議優先級${plain}"
+    read -p "請輸入要設置 IP 優先級的網卡名稱（例如 eth0）: " selected_nic
+
+    # 檢查輸入的網卡是否存在
+    if ! ifconfig | grep -q "$selected_nic"; then
+        echo -e "${red}輸入的網卡 ${selected_nic} 不存在，請重新輸入！${plain}"
+        continue
     fi
+
+    # 顯示菜單
+    echo -e "${yellow}正在設置 ${selected_nic} 的 IP 優先級...${plain}"
     echo "----------------------------------------"
     echo -e "${green}請選擇優先使用的 IP 協議：${plain}"
     echo -e "${green}1.${plain} IPv4"
     echo -e "${green}2.${plain} IPv6"
-    echo -e "${green}3.${plain} 退出脚本"
+    echo -e "${green}3.${plain} 取消設置並返回上一級菜單"
     read -p "請輸入對應數字 [1-3]: " selected_option
 
+    # 根據用戶選擇設置對應的優先級
     case "$selected_option" in
         1)
-            # 设置 IPv4 优先级
-            for nic in `cat nics.txt`
-            do
-                echo "${green}正在將 ${nic} 的 IPv4 優先級提高...${plain}"                
-                sed -i "/$nic/!d;s/^\(.*\)$/\1 precedence 1/g" /etc/gai.conf
-            done            
-            echo "${green}IPv4 優先級設置成功！${plain}"
+            echo -e "${green}正在將 ${selected_nic} 的 IPv4 優先級提高...${plain}"                
+            sed -i "/$selected_nic/!d;s/^\(.*\)$/\1 precedence 1/g" /etc/gai.conf          
+            echo -e "${green}${selected_nic} 的 IPv4 優先級設置成功！${plain}"
             ;;
         2)
-            # 设置 IPv6 优先级
-            for nic in `cat nics.txt`
-            do
-                echo "${green}正在將 ${nic} 的 IPv6 優先級提高...${plain}"                
-                sed -i "/$nic/!d;s/^\(.*\)$/\1 precedence 2/g" /etc/gai.conf
-            done            
-            echo "${green}IPv6 優先級設置成功！${plain}"
+            echo -e "${green}正在將 ${selected_nic} 的 IPv6 優先級提高...${plain}"                
+            sed -i "/$selected_nic/!d;s/^\(.*\)$/\1 precedence 2/g" /etc/gai.conf            
+            echo -e "${green}${selected_nic} 的 IPv6 優先級設置成功！${plain}"
             ;;
         3)
-            # 退出脚本
-            rm -f ./nics.txt
-            exit 0
+            break
             ;;
         *)
-            # 输入错误提示
+            # 輸入錯誤提示
             echo -e "${red}輸入錯誤，請重新輸入！${plain}"
             ;;
     esac
